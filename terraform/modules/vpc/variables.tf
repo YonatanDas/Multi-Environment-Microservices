5173fd5ace116c009356367b9517546e0e591595
YonatanDas <yonatan804@gmail.com>
2025-10-18 19:06:46 +0300

    feat(terraform): add VPC and networking configuration module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "env" {}
+variable "vpc_cidr" {}
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+variable "availability_zones" {
+  type = list(string)
+}
