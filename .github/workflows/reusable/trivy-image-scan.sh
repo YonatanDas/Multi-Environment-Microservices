6e2e44c83b6d5472199b29dd74c3e9249c225e05
YonatanDas <yonatan804@gmail.com>
2025-11-08 20:11:31 +0200

    testing

--- .github/workflows/reusable/trivy-image-scan.sh
+++ .github/workflows/reusable/trivy-image-scan.sh
@@ -7,8 +7,6 @@ AWS_REGION=$3
 
 echo "🧪 Running Trivy image scan for ${SERVICE}..."
 
-# Login again (in case cache cleared)
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$REGISTRY"
 
 trivy image \
   --exit-code 0 \
