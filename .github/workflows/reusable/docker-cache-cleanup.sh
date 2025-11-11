6b5820f8a118041b73c23ed3af2e4d9a8261b1b5
Yonatan Desaleyn <yonatan804@gmail.com>
2025-11-11 09:09:48 +0200

    testing

--- .github/workflows/reusable/docker-cache-cleanup.sh
+++ .github/workflows/reusable/docker-cache-cleanup.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "🧹 Cleaning up Docker build cache..."
-rm -rf /gha/.buildx-cache
+rm -rf /tmp/.buildx-cache
 mv /tmp/.buildx-cache-new /tmp/.buildx-cache
 echo "✅ Docker cache updated."
